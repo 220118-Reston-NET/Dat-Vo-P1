5c8cdbbe5d8e6c33e2f63c326ce5a4b9f9fabd8b
Hy0tic <vothanhdat278@gmail.com>
2022-03-01 16:50:38 -0500

    Update Testing

--- ProjectBL/ProjectBusinessLogic.cs
+++ ProjectBL/ProjectBusinessLogic.cs
@@ -207,267 +207,267 @@ namespace ProjectBL
 
 
 
-    public class ProjectBLitem : IProjectBLitem
-    {
-        private IRepository _repo;
-        public ProjectBLitem(IRepository p_repo)
-        {
-            _repo = p_repo;
-        }
-        public ItemModel AddItem(ItemModel p_Item)
-        {
-            List<ItemModel> listOfItem = _repo.GetAllItem();
-            return _repo.AddItem(p_Item);
-        }
+    // public class ProjectBLitem : IProjectBLitem
+    // {
+    //     private IRepository _repo;
+    //     public ProjectBLitem(IRepository p_repo)
+    //     {
+    //         _repo = p_repo;
+    //     }
+    //     public ItemModel AddItem(ItemModel p_Item)
+    //     {
+    //         List<ItemModel> listOfItem = _repo.GetAllItem();
+    //         return _repo.AddItem(p_Item);
+    //     }
 
-        public List<ItemModel> GetAllItem()
-        {
-            return _repo.GetAllItem();
-        }
+    //     public List<ItemModel> GetAllItem()
+    //     {
+    //         return _repo.GetAllItem();
+    //     }
 
-        public List<StoreFrontModel> GetAllStoreFront()
-        {
-            /// <summary>
-            /// return store front to add empty inventory
-            /// </summary>
-            /// <returns></returns>
-            return _repo.GetAllStoreFront();
-        }
+    //     public List<StoreFrontModel> GetAllStoreFront()
+    //     {
+    //         /// <summary>
+    //         /// return store front to add empty inventory
+    //         /// </summary>
+    //         /// <returns></returns>
+    //         return _repo.GetAllStoreFront();
+    //     }
 
-        public ItemModel RemoveItem(ItemModel p_Item)
-        {
-            return _repo.RemoveItem(p_Item);
-        }
+    //     public ItemModel RemoveItem(ItemModel p_Item)
+    //     {
+    //         return _repo.RemoveItem(p_Item);
+    //     }
 
-        public List<ItemModel> SearchItem(string p_name)
-        {
-            List<ItemModel> listOfItem = _repo.GetAllItem();
-            // LINQ library
-            return listOfItem
-                        .Where(item => item.ItemName.Contains(p_name)) //Where method is designed to filter a collection based on a condition
-                        .ToList(); //ToList method just converts into a list collection that the method needs to return
-        }
-        public InventoryModel AddInventory(InventoryModel p_inventory)
-        {
-            return _repo.AddInventory(p_inventory);
-        }
+    //     public List<ItemModel> SearchItem(string p_name)
+    //     {
+    //         List<ItemModel> listOfItem = _repo.GetAllItem();
+    //         // LINQ library
+    //         return listOfItem
+    //                     .Where(item => item.ItemName.Contains(p_name)) //Where method is designed to filter a collection based on a condition
+    //                     .ToList(); //ToList method just converts into a list collection that the method needs to return
+    //     }
+    //     public InventoryModel AddInventory(InventoryModel p_inventory)
+    //     {
+    //         return _repo.AddInventory(p_inventory);
+    //     }
 
-        public ItemModel GetItem(int itemID)
-        {
-            return _repo.GetItem(itemID);
-        }
-    }
+    //     public ItemModel GetItem(int itemID)
+    //     {
+    //         return _repo.GetItem(itemID);
+    //     }
+    // }
 
-    public class ProjectBLCustomer : IProjectBLCustomer
-    {
-        private IRepository _repo;
-        public ProjectBLCustomer(IRepository p_repo)
-        {
-            _repo = p_repo;
-        }
-        public CustomerModel AddCustomer(CustomerModel p_customer)
-        {
-            List<CustomerModel> listOfCustomer = _repo.GetAllCustomer();
-            if (p_customer.name.Count() < 2)
-            {
-                throw new Exception("Please enter a name");
-            }
-            else if (p_customer.phonenumber.Count() != 10)
-            {
-                throw new Exception("Please enter a valid phone number");
-            }
-            else if (!p_customer.email.Contains("@") )
-            {
-                throw new Exception("Please enter a valid email");
-            }
+    // public class ProjectBLCustomer : IProjectBLCustomer
+    // {
+    //     private IRepository _repo;
+    //     public ProjectBLCustomer(IRepository p_repo)
+    //     {
+    //         _repo = p_repo;
+    //     }
+    //     public CustomerModel AddCustomer(CustomerModel p_customer)
+    //     {
+    //         List<CustomerModel> listOfCustomer = _repo.GetAllCustomer();
+    //         if (p_customer.name.Count() < 2)
+    //         {
+    //             throw new Exception("Please enter a name");
+    //         }
+    //         else if (p_customer.phonenumber.Count() != 10)
+    //         {
+    //             throw new Exception("Please enter a valid phone number");
+    //         }
+    //         else if (!p_customer.email.Contains("@") )
+    //         {
+    //             throw new Exception("Please enter a valid email");
+    //         }
     
-            return _repo.AddCustomer(p_customer);
-        }
+    //         return _repo.AddCustomer(p_customer);
+    //     }
 
-        public List<CustomerModel> GetAllCustomer()
-        {
-            return _repo.GetAllCustomer();
-        }
+    //     public List<CustomerModel> GetAllCustomer()
+    //     {
+    //         return _repo.GetAllCustomer();
+    //     }
 
-        public CustomerModel RemoveCustomer(CustomerModel p_customer)
-        {
-            return _repo.RemoveCustomer(p_customer);
-        }
+    //     public CustomerModel RemoveCustomer(CustomerModel p_customer)
+    //     {
+    //         return _repo.RemoveCustomer(p_customer);
+    //     }
 
-        public List<CustomerModel> SearchCustomer(string p_name)
-        {
-            List<CustomerModel> listOfCustomer= _repo.GetAllCustomer();
-            // LINQ library
-            return listOfCustomer
-                        .Where(customer => customer.name.Contains(p_name)) //Where method is designed to filter a collection based on a condition
-                        .ToList(); //ToList method just converts into a list collection that the method needs to return
-        }
-    }
+    //     public List<CustomerModel> SearchCustomer(string p_name)
+    //     {
+    //         List<CustomerModel> listOfCustomer= _repo.GetAllCustomer();
+    //         // LINQ library
+    //         return listOfCustomer
+    //                     .Where(customer => customer.name.Contains(p_name)) //Where method is designed to filter a collection based on a condition
+    //                     .ToList(); //ToList method just converts into a list collection that the method needs to return
+    //     }
+    // }
 
-    public class ProjectBLStoreFront : IProjectBLStoreFront
-    {
-        private IRepository _repo;
-        public ProjectBLStoreFront(IRepository p_repo)
-        {
-            _repo = p_repo;
-        }
-        public StoreFrontModel AddStoreFront(StoreFrontModel p_storefront)
-        {
-            throw new NotImplementedException();
-        }
+    // public class ProjectBLStoreFront : IProjectBLStoreFront
+    // {
+    //     private IRepository _repo;
+    //     public ProjectBLStoreFront(IRepository p_repo)
+    //     {
+    //         _repo = p_repo;
+    //     }
+    //     public StoreFrontModel AddStoreFront(StoreFrontModel p_storefront)
+    //     {
+    //         throw new NotImplementedException();
+    //     }
 
-        public List<StoreFrontModel> GetAllStoreFront()
-        {
-            return _repo.GetAllStoreFront();
-        }
+    //     public List<StoreFrontModel> GetAllStoreFront()
+    //     {
+    //         return _repo.GetAllStoreFront();
+    //     }
 
-        public StoreFrontModel RemoveStoreFront(StoreFrontModel p_storefront)
-        {
-            throw new NotImplementedException();
-        }
+    //     public StoreFrontModel RemoveStoreFront(StoreFrontModel p_storefront)
+    //     {
+    //         throw new NotImplementedException();
+    //     }
 
-        public StoreFrontModel SearchStoreFront(string p_name)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class ProjectBLInventory : IProjectBLInventory
-    {
-        private IRepository _repo;
-        public ProjectBLInventory(IRepository p_repo)
-        {
-            _repo = p_repo;
-        }
-        public InventoryModel AddInventory(InventoryModel p_inventory)
-        {
-            throw new NotImplementedException();
-        }
+    //     public StoreFrontModel SearchStoreFront(string p_name)
+    //     {
+    //         throw new NotImplementedException();
+    //     }
+    // }
+    // public class ProjectBLInventory : IProjectBLInventory
+    // {
+    //     private IRepository _repo;
+    //     public ProjectBLInventory(IRepository p_repo)
+    //     {
+    //         _repo = p_repo;
+    //     }
+    //     public InventoryModel AddInventory(InventoryModel p_inventory)
+    //     {
+    //         throw new NotImplementedException();
+    //     }
 
-        public List<InventoryModel> GetAllInventory()
-        {
-            return _repo.GetAllInventory();
-        }
+    //     public List<InventoryModel> GetAllInventory()
+    //     {
+    //         return _repo.GetAllInventory();
+    //     }
 
-        public InventoryModel RemoveInventory(InventoryModel p_inventory)
-        {
-            throw new NotImplementedException();
-        }
+    //     public InventoryModel RemoveInventory(InventoryModel p_inventory)
+    //     {
+    //         throw new NotImplementedException();
+    //     }
 
-        public List<InventoryModel> SearchInventoryByStoreID(int storeID)
-        {
-            List<InventoryModel> listOfInventory= _repo.GetAllInventory();
-            return listOfInventory;
-        }
+    //     public List<InventoryModel> SearchInventoryByStoreID(int storeID)
+    //     {
+    //         List<InventoryModel> listOfInventory= _repo.GetAllInventory();
+    //         return listOfInventory;
+    //     }
 
-        public List<ItemModel> GetAllItem()
-        {
-            return _repo.GetAllItem();
-        }
+    //     public List<ItemModel> GetAllItem()
+    //     {
+    //         return _repo.GetAllItem();
+    //     }
 
-        public List<ItemModel> SearchItem(int itemID)
-        {
-            List<ItemModel> listOfItem= _repo.GetAllItem();
-            //public ItemModel result;
-            // ItemModel result = from item in listOfItem
-            //                     where item.ItemID.
+    //     public List<ItemModel> SearchItem(int itemID)
+    //     {
+    //         List<ItemModel> listOfItem= _repo.GetAllItem();
+    //         //public ItemModel result;
+    //         // ItemModel result = from item in listOfItem
+    //         //                     where item.ItemID.
 
-            // LINQ library
-            return listOfItem
-                        .Where(item => item.ItemID.Equals(itemID)) //Where method is designed to filter a collection based on a condition
-                        .ToList(); //ToList method just converts into a list collection that the method needs to return
-        }
+    //         // LINQ library
+    //         return listOfItem
+    //                     .Where(item => item.ItemID.Equals(itemID)) //Where method is designed to filter a collection based on a condition
+    //                     .ToList(); //ToList method just converts into a list collection that the method needs to return
+    //     }
 
-        public List<StoreFrontModel> GetAllStoreFront()
-        {
-            return _repo.GetAllStoreFront();
-        }
+    //     public List<StoreFrontModel> GetAllStoreFront()
+    //     {
+    //         return _repo.GetAllStoreFront();
+    //     }
 
-        public InventoryModel UpdateInventory(InventoryModel p_inventory)
-        {
-            return _repo.UpdateInventory(p_inventory);
-        }
+    //     public InventoryModel UpdateInventory(InventoryModel p_inventory)
+    //     {
+    //         return _repo.UpdateInventory(p_inventory);
+    //     }
 
-        public ItemModel GetItem(int itemID)
-        {
-            return _repo.GetItem(itemID);
-        }
+    //     public ItemModel GetItem(int itemID)
+    //     {
+    //         return _repo.GetItem(itemID);
+    //     }
 
-        public OrderModel AddOrder(OrderModel order)
-        {
+    //     public OrderModel AddOrder(OrderModel order)
+    //     {
 
-            CurrentCustomer.currentOrder.customerID = CurrentCustomer.currentcustomer.customerID;
-            CurrentCustomer.currentOrder.storeID = CurrentCustomer.currentstore.storeID;
+    //         CurrentCustomer.currentOrder.customerID = CurrentCustomer.currentcustomer.customerID;
+    //         CurrentCustomer.currentOrder.storeID = CurrentCustomer.currentstore.storeID;
 
-            int c = new int();
-            decimal total = new decimal();
-            foreach(var item in CurrentCustomer.currentcart)
-            {
-                total = total + item.ItemPrice*CurrentCustomer.currentcartquantity[c];
+    //         int c = new int();
+    //         decimal total = new decimal();
+    //         foreach(var item in CurrentCustomer.currentcart)
+    //         {
+    //             total = total + item.ItemPrice*CurrentCustomer.currentcartquantity[c];
                 
-                int currentquantity = new int();
-                List<InventoryModel> ListOfInventory = GetAllInventory();
-                foreach(var inven in ListOfInventory)
-                {
-                    if (inven.itemID == item.ItemID && inven.storeID == CurrentCustomer.currentstore.storeID)
-                    {
-                        currentquantity = inven.quantity;
-                    }
-                }
+    //             int currentquantity = new int();
+    //             List<InventoryModel> ListOfInventory = GetAllInventory();
+    //             foreach(var inven in ListOfInventory)
+    //             {
+    //                 if (inven.itemID == item.ItemID && inven.storeID == CurrentCustomer.currentstore.storeID)
+    //                 {
+    //                     currentquantity = inven.quantity;
+    //                 }
+    //             }
 
-                CurrentCustomer.currentinventory.itemID = item.ItemID;
-                CurrentCustomer.currentinventory.storeID = CurrentCustomer.currentstore.storeID;
-                CurrentCustomer.SetInventoryQuantity(currentquantity-CurrentCustomer.currentcartquantity[c]);
-                _repo.UpdateInventory(CurrentCustomer.currentinventory);
+    //             CurrentCustomer.currentinventory.itemID = item.ItemID;
+    //             CurrentCustomer.currentinventory.storeID = CurrentCustomer.currentstore.storeID;
+    //             CurrentCustomer.SetInventoryQuantity(currentquantity-CurrentCustomer.currentcartquantity[c]);
+    //             _repo.UpdateInventory(CurrentCustomer.currentinventory);
 
-                c=c+1;
-            }
-            CurrentCustomer.currentOrder.TotalPrice = total;
+    //             c=c+1;
+    //         }
+    //         CurrentCustomer.currentOrder.TotalPrice = total;
 
-            //CurrentCustomer.currentcart.Clear();
-            //CurrentCustomer.currentcartquantity.Clear();
+    //         //CurrentCustomer.currentcart.Clear();
+    //         //CurrentCustomer.currentcartquantity.Clear();
 
 
-            return _repo.AddOrder(order);
-        }
+    //         return _repo.AddOrder(order);
+    //     }
 
-        public List<OrderModel> GetAllOrder()
-        {
-            return _repo.GetAllOrder();
-        }
+    //     public List<OrderModel> GetAllOrder()
+    //     {
+    //         return _repo.GetAllOrder();
+    //     }
 
-        public void AddOrderItem()
-        {
-            List<OrderModel> listOfOrder = _repo.GetAllOrder();
-            int c = new int();
-            foreach(var item in CurrentCustomer.currentcart)
-            {
-                OrderItemModel OI = new OrderItemModel();
-                OI.itemID = item.ItemID;
-                OI.orderID = listOfOrder.Last().orderID;
-                OI.quantity = CurrentCustomer.currentcartquantity[c];
-                _repo.AddOrderItem(OI);
-                c = c + 1;
-            }
+    //     public void AddOrderItem()
+    //     {
+    //         List<OrderModel> listOfOrder = _repo.GetAllOrder();
+    //         int c = new int();
+    //         foreach(var item in CurrentCustomer.currentcart)
+    //         {
+    //             OrderItemModel OI = new OrderItemModel();
+    //             OI.itemID = item.ItemID;
+    //             OI.orderID = listOfOrder.Last().orderID;
+    //             OI.quantity = CurrentCustomer.currentcartquantity[c];
+    //             _repo.AddOrderItem(OI);
+    //             c = c + 1;
+    //         }
 
-            CurrentCustomer.currentcart.Clear();
-            CurrentCustomer.currentcartquantity.Clear();
-        }
+    //         CurrentCustomer.currentcart.Clear();
+    //         CurrentCustomer.currentcartquantity.Clear();
+    //     }
 
-        public CustomerModel GetCustomer(int customerID)
-        {
-            return _repo.SearchCustomerByID(customerID);
-        }
+    //     public CustomerModel GetCustomer(int customerID)
+    //     {
+    //         return _repo.SearchCustomerByID(customerID);
+    //     }
 
-        public StoreFrontModel GetStore(int storeID)
-        {
-            return _repo.SearchStoreByID(storeID);
-        }
+    //     public StoreFrontModel GetStore(int storeID)
+    //     {
+    //         return _repo.SearchStoreByID(storeID);
+    //     }
 
-        public List<OrderItemModel> SearchOrderItem(int orderID)
-        {
-            return _repo.SearchOrderItem(orderID);
-        }
-    }
+    //     public List<OrderItemModel> SearchOrderItem(int orderID)
+    //     {
+    //         return _repo.SearchOrderItem(orderID);
+    //     }
+    // }
 
 
 
